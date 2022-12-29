1c2f2a39d5e33a9d43813d69610d9ff37f5d1a50
Anocha Lengsiri <anocha@renthub.in.th>
2022-12-29 19:26:27 +0700

    init

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Layout pageName={pageProps.pageName}>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const Home = (props) => {
+  return <>fff</>;
+};
+
+Home.getInitialProps = async (ctx) => {
+  return { pageName: "HomeTest" };
+};
+export default Home;
